53233df34b33312625354bc4e8812f65d2177e89
Stephen Smith <stephenlehanesmith@gmail.com>
2022-07-06 06:02:07 -0700

    changed name in .gitignore and wrote basic function to get top comments from a reddit post

--- Voight-Kampff-Test-Backend/scraping.py
+++ Voight-Kampff-Test-Backend/scraping.py
@@ -1,10 +1,26 @@
-import secrets
-import praw
+import code
 
-print(secrets.reddit_client_id)
-def get_top_level_posts():
+from requests_toolbelt import user_agent
+import praw
+import pandas as pd
+
+def get_top_level_posts(url):
     reddit = praw.Reddit(
-    client_id=secrets.reddit_client_id,
-    client_secret=secrets.reddit_secret,
-    password=secrets.reddit_password,
-    username=secrets.reddit_username)
+        client_id=code.reddit_client_id,
+        client_secret=code.reddit_secret,
+        password=code.reddit_password,
+        user_agent=code.reddit_username)
+
+    df = pd.DataFrame(columns = ['Answer'])
+    submission = reddit.submission(url=url)
+    submission.comments.replace_more(limit=0)
+
+    for top_level_comment in submission.comments:
+        df = df.append({'Answer' : top_level_comment.body},
+                ignore_index = True)
+
+    return df
+
+if __name__ == '__main__.py':
+    url = 'https://www.reddit.com/r/AskReddit/comments/vnkxce/whats_a_weird_smell_youre_willing_to_admit_you/'
+    print(get_top_level_posts(url=url))
