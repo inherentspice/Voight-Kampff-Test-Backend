8da8e3aca2df3c6735ae6c2a8fadc59f01f5a0fa
Stephen Smith <stephenlehanesmith@gmail.com>
2022-07-06 22:09:11 -0700

    added the bones for a function to get all comments from a reddit post

--- Voight-Kampff-Test-Backend/scraping.py
+++ Voight-Kampff-Test-Backend/scraping.py
@@ -20,6 +20,21 @@ def get_top_level_posts(url):
     df.to_csv(f'raw_data/{title}')
     return df
 
-if __name__ == '__main__.py':
-    url = 'https://www.reddit.com/r/AskReddit/comments/vnkxce/whats_a_weird_smell_youre_willing_to_admit_you/'
-    print(get_top_level_posts(url=url))
+def get_child_posts(url):
+    reddit = praw.Reddit(
+        client_id=code.reddit_client_id,
+        client_secret=code.reddit_secret,
+        password=code.reddit_password,
+        user_agent=code.reddit_username)
+
+    df = pd.DataFrame(columns = ['Answer'])
+    submission = reddit.submission(url=url)
+    submission.comments.replace_more(limit=0)
+
+    for comment in submission.comments.list():
+        print(comment.body)
+
+# if __name__ == '__main__.py':
+url = 'https://www.reddit.com/r/AskReddit/comments/vnkxce/whats_a_weird_smell_youre_willing_to_admit_you/'
+    # print(get_top_level_posts(url=url))
+get_child_posts(url)
