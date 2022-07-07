b0ae7e5f86bd6a835301758830af32912b41b395
Stephen Smith <stephenlehanesmith@gmail.com>
2022-07-06 21:45:46 -0700

    added part so df is saved as a csv under raw_data

--- Voight-Kampff-Test-Backend/scraping.py
+++ Voight-Kampff-Test-Backend/scraping.py
@@ -16,9 +16,10 @@ def get_top_level_posts(url):
     for top_level_comment in submission.comments:
         df = df.append({'Answer' : top_level_comment.body},
                 ignore_index = True)
-
+    title = 'test'
+    df.to_csv(f'raw_data/{title}')
     return df
 
-# if __name__ == '__main__.py':
-url = 'https://www.reddit.com/r/AskReddit/comments/vnkxce/whats_a_weird_smell_youre_willing_to_admit_you/'
-print(get_top_level_posts(url=url))
+if __name__ == '__main__.py':
+    url = 'https://www.reddit.com/r/AskReddit/comments/vnkxce/whats_a_weird_smell_youre_willing_to_admit_you/'
+    print(get_top_level_posts(url=url))
