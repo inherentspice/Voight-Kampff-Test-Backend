29c32991be8df31ddd98068d49c0e4e2f15882d3
Tshepo Kubeka <tkubeka1993@gmail.com>
2022-07-11 20:51:47 +0800

    training_data update

--- training_data.py
+++ training_data.py
@@ -1,42 +1,14 @@
-import os
 import pandas as pd
 
-#load csv once it's created
+df = pd.read_csv ('raw_data/Whats_a_job_thats_romanticized_but_in_reality_sucks?_all.csv')
 
-class Question:
-    '''
-    DataFrames containing all questions as indices,
-    and various properties of these questions as columns
-    '''
-    def __init__(self):
-        # Assign an attribute ".data" to all new instances
-        self.data = Olist().get_data()  #"Olist" will be replaced by the dataframe once the csv file is created
-
-     def get_question(self):
-        """
-        Returns a DataFrame with:
-        [questions]
-        and filters out non-delivered orders unless specified
-        """
-        # Hint: Within this instance method, you have access to the instance of the class Question in the variable self, as well as all its attributes
-        # make sure to create a copy rather than a "view"
-        questions = self.data['questions'].copy()
 
 class Response:
-    '''
-    DataFrames containing all orders as index,
-    and various properties of these orders as columns
-    '''
     def __init__(self):
-        # Assign an attribute ".data" to all new instances
-        self.data = Olist().get_data() #"Olist" will be replaced by the dataframe once the csv file is created
+        self.data = df['Answer'].get_data()
 
     def get_response(self):
-        """
-        Returns a DataFrame with:
-        [responses]
-        and filters out non-delivered orders unless specified
-        """
-        # Hint: Within this instance method, you have access to the instance of the class Question in the variable self, as well as all its attributes
-        # make sure to create a copy rather than a "view"
-        responses = self.data['responses'].copy()
+        responses = self.data['Answer'].copy()
+        return responses
+
+print (Response.get_response(df))
