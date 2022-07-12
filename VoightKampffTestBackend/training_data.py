5c36fd2adf97b788b86305eb8b51e2736ba75615
Stephen Smith <stephenlehanesmith@gmail.com>
2022-07-11 21:43:24 -0700

    added csv_path so can use training_data as a class to retrieve data

--- VoightKampffTestBackend/training_data.py
+++ VoightKampffTestBackend/training_data.py
@@ -7,7 +7,7 @@ class Data:
         pass
 
     def get_response(self):
-        csv_path = 'raw_data/scraped_data'
+        csv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'raw_data', 'scraped_data')
         file_names = [f for f in os.listdir(csv_path) if f.endswith('.csv')]
         files = file_names.copy()
 
