ce2737f6cccdf4675eb96713f5fdab5dfadac81b
Stephen Smith <stephenlehanesmith@gmail.com>
2022-07-13 01:44:04 -0700

    added question to each response and filled na values

--- VoightKampffTestBackend/training_data.py
+++ VoightKampffTestBackend/training_data.py
@@ -25,7 +25,9 @@ class Data:
     def transform_training_data(self):
 
         data = self.get_response()
+
         data.to_csv('raw_data/training_files.txt', header=None, index=None, sep=' ', mode='a')
+
         return
 
 
