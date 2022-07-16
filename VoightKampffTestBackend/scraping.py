d4f9ed7e7c38ecbd4eb96cfbc48ad448ecc15418
Stephen Smith <stephenlehanesmith@gmail.com>
2022-07-15 23:22:31 -0700

    Added specifications to Dockerfile, and added function to scraping file to scrape top ten hottest posts from reddit

--- VoightKampffTestBackend/scraping.py
+++ VoightKampffTestBackend/scraping.py
@@ -1,6 +1,7 @@
 import VoightKampffTestBackend.gettit as gettit
 import praw
 import pandas as pd
+import time
 
 def get_top_level_posts(url):
     codes = gettit.Codes()
@@ -18,6 +19,7 @@ def get_top_level_posts(url):
         df = df.append({'Answer' : top_level_comment.body},
                 ignore_index = True)
     title = submission.title
+    title = title.replace("/", "")
     df.to_csv(f'raw_data/scraped_data/{title}.csv')
     return df
 
@@ -40,6 +42,23 @@ def get_child_posts(url):
     df.to_csv(f'raw_data/scraped_data/{title}_all.csv')
     return df
 
+def get_hot_posts(subreddit='AskReddit'):
+    codes = gettit.Codes()
+    reddit = praw.Reddit(
+        client_id=codes.reddit_client_id,
+        client_secret=codes.reddit_secret,
+        password=codes.reddit_password,
+        user_agent=codes.reddit_username)
+
+    posts = []
+    ml_subreddit = reddit.subreddit(subreddit)
+    for post in ml_subreddit.hot(limit=10):
+        posts.append(post.url)
+    for i in posts:
+        print(f"Getting answers...")
+        get_top_level_posts(i)
+        time.sleep(0.10)
+
 if __name__ == '__main__':
     URLS = ["https://www.reddit.com/r/AskReddit/comments/vo73uc/whats_a_weird_thing_you_think_only_you_do/",
             "https://www.reddit.com/r/AskReddit/comments/vjpkye/the_supreme_court_has_overturned_roe_v_wade_how/?utm_source=share&utm_medium=web2x&context=3 ",
@@ -53,4 +72,5 @@ if __name__ == '__main__':
             ]
 
     for i in URLS:
-        get_child_posts(url=i)
+        # get_child_posts(url=i)
+        get_hot_posts()
