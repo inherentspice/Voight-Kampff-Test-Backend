3b7612346a479a0c378a6c5a0658b6a9f7ed2f48
Stephen Smith <stephenlehanesmith@gmail.com>
2022-07-18 21:22:00 -0700

    added function to search 'all' on reddit for specific keywords

--- VoightKampffTestBackend/scraping.py
+++ VoightKampffTestBackend/scraping.py
@@ -2,6 +2,7 @@ import VoightKampffTestBackend.gettit as gettit
 import praw
 import pandas as pd
 import time
+import os
 
 def get_top_level_posts(url):
     codes = gettit.Codes()
@@ -12,13 +13,13 @@ def get_top_level_posts(url):
         user_agent=codes.reddit_username)
 
     df = pd.DataFrame(columns = ['Answer'])
-    submission = reddit.submission(url=url)
-    submission.comments.replace_more(limit=0)
+    link = reddit.submission(url=url)
+    link.comments.replace_more(limit=0)
 
-    for top_level_comment in submission.comments:
+    for top_level_comment in link.comments:
         df = df.append({'Answer' : top_level_comment.body},
                 ignore_index = True)
-    title = submission.title
+    title = link.title
     title = title.replace("/", "")
     df.to_csv(f'raw_data/scraped_data/{title}.csv')
     return df
@@ -31,15 +32,21 @@ def get_child_posts(url):
         password=codes.reddit_password,
         user_agent=codes.reddit_username)
 
-    df = pd.DataFrame(columns = ['Answer'])
-    submission = reddit.submission(url=url)
-    submission.comments.replace_more(limit=0)
+    path = f'raw_data/scraped_data/qanda'
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print(f"Making new directory at {path}")
 
-    for comment in submission.comments.list():
+
+    df = pd.DataFrame(columns = ['Answer'])
+    link = reddit.submission(url=url)
+    link.comments.replace_more(limit=0)
+
+    for comment in link.comments.list():
         df = df.append({'Answer' : comment.body},
                 ignore_index = True)
-    title = submission.title
-    df.to_csv(f'raw_data/scraped_data/{title}_all.csv')
+    title = link.title
+    df.to_csv(f'raw_data/scraped_data/qanda/{title}.csv')
     return df
 
 def get_hot_posts(subreddit='AskReddit'):
@@ -50,14 +57,51 @@ def get_hot_posts(subreddit='AskReddit'):
         password=codes.reddit_password,
         user_agent=codes.reddit_username)
 
+    path = f'raw_data/scraped_data/misc'
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print(f"Making new directory at {path}")
     posts = []
-    ml_subreddit = reddit.subreddit(subreddit)
-    for post in ml_subreddit.hot(limit=10):
+    link = reddit.subreddit(subreddit)
+    for post in link.hot(limit=10):
         posts.append(post.url)
-    for i in posts:
+    for post in posts:
         print(f"Getting answers...")
-        get_top_level_posts(i)
+        get_top_level_posts(post)
         time.sleep(0.10)
+
+def search_by_keyword(subreddit="all", search_term="wealth"):
+    codes = gettit.Codes()
+    reddit = praw.Reddit(
+        client_id=codes.reddit_client_id,
+        client_secret=codes.reddit_secret,
+        password=codes.reddit_password,
+        user_agent=codes.reddit_username)
+
+    path = f"raw_data/scraped_data/{search_term}"
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print(f"Making new directory at {path}")
+
+    link = reddit.subreddit(subreddit)
+    df = pd.DataFrame(columns=['Answer'])
+    for post in link.search(search_term, limit=1000):
+        print(f"Getting answers...")
+        # get_top_level_posts(post.title)
+        post.comments.replace_more(limit=0)
+        title = post.title[0:12]
+        file_path = f"raw_data/scraped_data/{search_term}/{title}.csv"
+        if os.path.isfile(file_path):
+            print(f"{title}.csv already exists...")
+        else:
+            for comment in post.comments.list():
+                df = df.append({'Answer' : comment.body},
+                    ignore_index = True)
+            df.to_csv(f'raw_data/scraped_data/{search_term}/{title}.csv')
+        time.sleep(0.10)
+
+
+
 
 if __name__ == '__main__':
     URLS = ["https://www.reddit.com/r/AskReddit/comments/vo73uc/whats_a_weird_thing_you_think_only_you_do/",
@@ -72,5 +116,6 @@ if __name__ == '__main__':
             ]
 
     for i in URLS:
-        # get_child_posts(url=i)
-        get_hot_posts()
+        get_child_posts(url=i)
+    get_hot_posts()
+    search_by_keyword()
