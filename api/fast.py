5a9c6ee9ab32e29ceeb36c19e355a20b046e3a98
Tshepo Kubeka <tkubeka1993@gmail.com>
2022-07-30 14:25:07 +0800

    altered minimum and maximum length of responses based on Data Analysis findings

--- api/fast.py
+++ api/fast.py
@@ -83,8 +83,9 @@ def question():
 
 @app.get("/response")
 async def response(question):
+    question_length = question.str.split().len()
     sess = await response_generator.Response().get_model(run_name='run2')
-    length = np.random.randint(50, 100)
+    length = np.random.randint(question_length + 32, 160)
     top_k = np.random.randint(2, 6000)
     temperature = np.random.uniform(0.6, 0.9)
 
