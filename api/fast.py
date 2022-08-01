9801b96e36d2800ced28d13f482b9b02ddd39a30
Stephen Smith <stephenlehanesmith@gmail.com>
2022-08-01 06:11:36 -0700

    removed erroneous await call

--- api/fast.py
+++ api/fast.py
@@ -94,7 +94,7 @@ def question():
 
 def response(question):
     question_length = question.str.split().len()
-    sess = await response_generator.Response().get_model(run_name='run2')
+    sess = response_generator.Response().get_model(run_name='run2')
     length = np.random.randint(question_length + 32, 160)
     top_k = np.random.randint(2, 6000)
     temperature = np.random.uniform(0.6, 0.9)
